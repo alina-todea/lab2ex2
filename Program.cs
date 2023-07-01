225bd60b3989dfef5dd534138b961137ebcef0dc
alina-todea <137332096+alina-todea@users.noreply.github.com>
2023-07-01 15:33:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Lab2ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * Scrieti o functie care va determina daca un numar este sau nu palindrom.
+             */
+
+            Console.WriteLine("Introduceti un numar intreg");
+            int numar = int.Parse(Console.ReadLine());
+
+            if (EstePalindrom(numar))
+            {
+                Console.WriteLine("Numarul " + numar + " este palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Numarul " + numar + " nu este palindrom");
+            }
+
+        }
+
+        static bool EstePalindrom(int numar)
+        {
+            bool result = false;
+            int intermediar = numar;
+            int numarNou = 0;
+            int ultimaCifra = 0;
+
+
+            while (intermediar>0)
+            {
+                ultimaCifra = intermediar % 10;
+                numarNou = (numarNou * 10) + ultimaCifra;
+                intermediar = intermediar / 10;
+
+            }
+
+            if (numarNou == numar)
+            {
+                result = true;
+            }
+            
+            return result;
+
+        }
+    }
+}
